b38a916b891aab9e7698f482410424343c7039df
Jenil Desai <jenildev91@gmail.com>
2025-07-11 23:48:50 +0530

    v1.3.1
    - Add ThemeProvider component and update Providers to include it; suppress hydration warnings in RootLayout.

--- components/global/Providers.tsx
+++ components/global/Providers.tsx
@@ -1,7 +1,14 @@
 "use client";
 
 import { Provider } from "jotai";
+import { ThemeProvider } from "./ThemeProvider";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <Provider>{children}</Provider>;
+  return (
+    <Provider>
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+        {children}
+      </ThemeProvider>
+    </Provider>
+  );
 }
