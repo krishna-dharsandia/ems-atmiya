a4adac37487ad8ee5ce9972480354c6cd665c04b
krishna-dharsandia <kp064342@gmail.com>
2025-10-08 14:14:58 +0530

    fix: remove unnecessary marginTop from styles in HackthonVolunteerICARDBunch component

--- components/export/HacktthonVolunteerICARDBunch.tsx
+++ components/export/HacktthonVolunteerICARDBunch.tsx
@@ -181,7 +181,6 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         justifyContent: 'center',
         alignItems: 'center',
-        marginTop: 10,
         marginBottom: '0.1mm',
         gap: '0.1mm',
     },
